8d4b58d03a6bbf8beac7cecbbdc47bd53403f54d
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-10-21 12:28:11 -0400

    a

---
+++ index.js
@@ -0,0 +1,45 @@
+for (let i = 0; i < 7; i++) {
+  //i === 0
+  console.log(i);
+}
+
+let x = [1, 2, 3, 4];
+x.forEach((i) => console.log(i));
+
+/* let HP = 100;
+while (HP > 0) {
+  console.log("select fight commadn");
+  console.log("randomly select enemy command");
+  console.log("subtract health");
+  userFight();
+  aiFight();
+  calcDamage();
+} */
+
+let n = 12;
+function factorialNumber(n) {
+  if (n <= 0) {
+    return "negative number or zero";
+  }
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+    //result = result * i
+  }
+  return result;
+}
+
+console.log(factorialNumber(5));
+
+function newFactorial(n) {
+  if (n > 0) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+      //result = result * i
+    }
+    return result;
+  } else {
+    return "something went wrong";
+  }
+}
---
+++ test.md
@@ -0,0 +1,25 @@
+```javascript
+if (input == 0) {
+  output = "rock";
+} else if (input == 1) {
+  output = "paper";
+} else {
+  output = "scissors";
+}
+```
+
+This is the selection
+
+```javascript
+for (let i = 0; i <= random; i++) {
+  if (i % 3 == 0) {
+    opponentChoice = "rock";
+  } else if (i % 2 == 0) {
+    opponentChoice = "paper";
+  } else {
+    opponentChoice = "scissors";
+  }
+}
+```
+
+This is the Iteration

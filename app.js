1d0212b19b9d0afa5924962115a0d288f7f416e6
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-10-24 12:28:48 -0400

    a

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function occupiedBoth(yesterday, today) {
 }
 occupiedBoth(); */
 
-function getSpaces(spaces, yes, today) {
+/* function getSpaces(spaces, yes, today) {
   let count = 0;
 
   for (let i = 0; i < spaces; i++) {
@@ -21,4 +21,31 @@ function getSpaces(spaces, yes, today) {
   }
   return count;
 }
-console.log(getSpaces(5, "CC..C", "C.C.C"));
+console.log(getSpaces(5, "CC..C", "C.C.C")); */
+
+//divide number by 2 until reach 0 (ignore remainder)
+//take the remainders and
+/* function decimalBinary(number) {
+    for (let i = 0; i > 0; i++) {
+        if (number % 2) 
+
+    }
+} */
+
+//start from leftmost digit - multiply the digit by the left digit + 1
+//use this product and continue
+
+function binaryDecimal(binary) {
+  let product = 0;
+  let adder = 1;
+  let decimalvalue = 1;
+  for (let i = binary.length - 1; i > 0; i--) {
+    if (binary[i] == "1") {
+      product = product + adder;
+      decimalvalue = product * 2 + adder;
+    }
+    adder = decimalvalue;
+  }
+  console.log(product);
+}
+binaryDecimal("00011");
